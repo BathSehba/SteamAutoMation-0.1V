5ce0ac818fdf066131de8df93538664d1edfe8f3
BathSehba <64278104+BathSehba@users.noreply.github.com>
2021-08-13 11:59:40 -0500

    Update accountdata-class-test.py
    Showing myself how classes work

--- accountdata-class-test.py
+++ accountdata-class-test.py
@@ -1,1 +1,11 @@
+class accountdata(object):
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
 
+
+user1 = accountdata("element", "1234password")
+user2 = accountdata("funnyguys", "24343password")
+
+print(user1.username)
+print((user2.password))
