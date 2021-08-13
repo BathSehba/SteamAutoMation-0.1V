fdd82378ad3b3e99ceee932b626d57e679e96096
BathSehba <64278104+BathSehba@users.noreply.github.com>
2021-08-13 16:07:03 -0500

    Update accountdata-class-test.py

--- accountdata-class-test.py
+++ accountdata-class-test.py
@@ -9,3 +9,25 @@ user2 = accountdata("funnyguys", "24343password")
 
 print(user1.username)
 print(user2.password)
+
+
+
+
+
+class accountdata(object):
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+
+
+user1 = accountdata("element", "1234password")
+user2 = accountdata("funnyguys", "24343password")
+
+uid = input('enter')
+if uid in user1.username:
+	print('already found')
+else:
+	print('will add')
+	
+	
+	Above will check to see if info is in userdata
