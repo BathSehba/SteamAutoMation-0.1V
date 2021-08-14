23306fc560d54ad750b4c4a500bf2657c5d275fb
BathSehba <64278104+BathSehba@users.noreply.github.com>
2021-08-14 17:58:11 -0600

    Update jsonstuff.py

--- jsonstuff.py
+++ jsonstuff.py
@@ -1,1 +1,8 @@
+with open(r'accountlist.json', ) as f:
+    acc_list = json.load(f)
+    print(acc_lsit["string of what you want to pull from JSON"[0-put array number here])   
+                   
+with open(r'accountlist.json', ) as f:
+    acc_list = json.load(f)
+    print(acc_lsit["userID"[0])   
 
